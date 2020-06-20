3c942baae231c60050f9203182a59267dc2f200d
Reese Kim <reesekimm@gmail.com>
2020-06-21 01:04:38 +0900

    feat: makeStyles api 사용하여 Header font-size 변경

--- src/views/components/Header.tsx
+++ src/views/components/Header.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { Grid, AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+
+const useStyles = makeStyles({
+  headerTextStyle: {
+    fontSize: "22px",
+  },
+});
 
 interface HeaderProps {
   title: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
+  const classes = useStyles();
   return (
     <Grid item>
       <AppBar position="static">
         <Toolbar>
           <ArrowBackIosIcon />
-          <Typography variant="subtitle1">{title}</Typography>
+          <Typography variant="h2" className={classes.headerTextStyle}>
+            {title}
+          </Typography>
         </Toolbar>
       </AppBar>
     </Grid>

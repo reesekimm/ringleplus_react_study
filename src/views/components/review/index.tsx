49003ca4fa843fc931040579c2d08cbb21716f83
Reese Kim <reesekimm@gmail.com>
2020-06-27 22:19:28 +0900

    feat: Suggestion 데이터 렌더링 및 스타일 적용

--- src/views/components/review/index.tsx
+++ src/views/components/review/index.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import Suggestions from "./Suggestions";
 import Feedback from "./Feedback";
 import Container from "@/common/Container";
 import * as options from "Const/containerOptions";
 
 const ReviewBody: React.FC = () => (
   <div className="review-body__wrapper">
-    <Grid item container spacing={2}>
-      <Grid item container xs={12} md={7} direction="column" spacing={2}>
+    <Grid item container spacing={2} style={{ height: "100%" }}>
+      <Grid item container xs={12} md={7} direction="column" spacing={2} justify="space-between">
         <Grid item container spacing={2}>
           <Grid item xs={12} sm={7}>
             <Container options={options.chat} />
           </Grid>
           <Grid item xs={12} sm={5}>
-            <Container options={options.suggestions} />
+            <Suggestions />
           </Grid>
         </Grid>
         <Grid item>audio controller</Grid>
       </Grid>
       <Grid item container xs={12} md={5} direction="column" spacing={2}>
-        <Feedback />
+        <Grid item>
+          <Feedback />
+        </Grid>
         <Grid item>
           <Container options={options.chart} />
         </Grid>
